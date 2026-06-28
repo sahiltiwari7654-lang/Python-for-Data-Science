79721f3e0dd8a151d6c890fd752feab5312d91db
sahiltiwari7654-lang <sahiltiwari7654@gmail.com>
2026-06-29 00:24:00 +0530

    Added project 4

---
+++ 02_Data_Structures/04_Project_4.py
@@ -0,0 +1,18 @@
+"""
+Program : Q01 - Count Alex Occurrences
+
+Topic : Strings
+
+Problem Statement:
+Given a string, count how many times the name "Alex"
+appears in the string.
+"""
+
+# Input string
+text = input("Enter a string: ")
+
+# Count occurrences of "Alex"
+count = text.count("Alex")
+
+# Display the result
+print("Number of times Alex appears:", count)
